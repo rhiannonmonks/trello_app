c99273ab8c20be1601c964c22b46e43b99d9d623
Toby Turnbull <turnbulltoby@gmail.com>
2016-01-15 12:51:16 +1100

    board collection, model, template. index

---
+++ app/assets/javascripts/collections/boards.js
@@ -0,0 +1,23 @@
+TrelloClone.Collections.Boards = Backbone.Collection.extend({
+  model: TrelloClone.Models.Board,
+  url: "/api/boards",
+
+
+  getOrFetch: function(id){
+    var board = this.get(id)
+    if (board){
+      board.fetch()
+    } else {
+      board = new TrelloClone.Models.Board({
+        id: id
+      })
+      board.fetch({
+        success: function(){
+          TrelloClone.Collections.boards.add(board);
+        }
+      });
+    }
+    return board;
+  }
+});
+TrelloClone.Collections.boards = new TrelloClone.Collections.Boards();
---
+++ app/assets/javascripts/views/boards_index.js
@@ -0,0 +1,11 @@
+TrelloClone.Views.BoardsIndex = Backbone.View.extend({
+  initialize: function(){
+    this.listenTo(this.collection, 'sync', this.render);
+  },
+  template: JST['boards/index'],
+  render: function(){
+    var content = this.template({boards: this.collection})
+    this.$el.html(content);
+    return this
+  }
+});
