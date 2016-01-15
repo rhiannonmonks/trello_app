9f04d8b7b9ffde48407e51536fbfd4fb11159d81
Toby Turnbull <turnbulltoby@gmail.com>
2016-01-15 15:19:42 +1100

    application.js

--- app/assets/javascripts/routers/boards_router.js
+++ app/assets/javascripts/routers/boards_router.js
@@ -3,5 +3,33 @@
 TrelloClone.Router.Boards = Backbone.Router.extend({
 routes: {
 "": 'boardsIndex',
+"boards/new": 'newBoard',
+"boards/:id": 'showBoard'
 },
+
+boardsIndex: function(){
+  TrelloClone.Collections.boards.fetch();
+  var boardIndexView = new TrelloClone.Views.BoardsIndex({
+    collection: TrelloClone.Collections.boards
+  });
+  this._swapView(boardIndexView);
+},
+newBoard: function(){
+  var boardNewView = new TrelloClone.Views.BoardNew();
+  this._swapView(boardNewView);
+},
+showBoard: function(id){
+  var board = TrelloClone.Collections.boards.getOrFetch(id);
+  var boardShowView = TrelloClone.Views.BoardShow({
+    model: board
+  });
+  this._swapView(boardShowView);
+},
+
+_swapView: function(newView){
+  this._currentView && this._currentView.remove();
+  this._currentView = newView;
+
+  $("#main").html(newView.render().$el);
+}
 });
